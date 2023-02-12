77ee5cf9b731d111be568e6dc7fe817a0e2bc921
David <lilcech@gmail.com>
2023-02-12 22:20:16 +0100

    fix styles

---
+++ src/components/icons/EnterIcon.tsx
@@ -0,0 +1,9 @@
+export const EnterIcon = (props: any) => (
+  <svg width="54" height="54" fill="none" xmlns="http://www.w3.org/2000/svg" {...props} >
+    <path
+      d="M15.98 25.5a1.5 1.5 0 0 0 0 3v-3Zm23.45 2.56a1.5 1.5 0 0 0 0-2.12l-9.54-9.55a1.5 1.5 0 1 0-2.12 2.12L36.25 27l-8.48 8.49a1.5 1.5 0 1 0 2.12 2.12l9.54-9.55Zm-23.45.44h22.4v-3h-22.4v3Z"
+      fill="#fff"
+    />
+    <rect x=".5" y=".5" width="53" height="53" rx="26.5" stroke="#fff" />
+  </svg>
+);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
-import localFont from '@next/font/local'
+// import localFont from '@next/font/local'
 import { type AppType } from "next/dist/shared/lib/utils"
 
 import "../styles/globals.css"
+import './main.css'
 
-const PPAgrandir = localFont({ src: '../styles/fonts/PPAgrandir-Variable.ttf' })
+// const PPAgrandir = localFont({ src: '../styles/fonts/PPAgrandir-Variable.ttf' }) as any
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <main className={PPAgrandir.className}><Component  {...pageProps} /></main>
+  return <main><Component  {...pageProps} /></main>
 }
 
 export default MyApp
